3e1e60c88c7051c4c9a2b204c63adb646409caf0
Netanel Levine <74298433+netanellevine@users.noreply.github.com>
2022-04-02 23:33:40 +0300

    Add files via upload

--- gamma.py
+++ gamma.py
@@ -11,20 +11,11 @@
 from cv2 import cv2 as cv
 import numpy as np
 title_window = 'Gamma Correction'
+trackbar_name = 'Gamma:'
 gamma_slider_max_val = 200
 max_pix = 255
 isColor = False
 img = 0
-
-
-def on_trackbar(val: int):
-    gamma = float(val) / 100
-    invGamma = 1000 if gamma == 0 else 1.0 / gamma
-    gammaTable = np.array([((i / float(max_pix)) ** invGamma) * max_pix
-                           for i in np.arange(0, max_pix + 1)]).astype("uint8")
-    img_ = cv.LUT(img, gammaTable)
-
-    cv.imshow(title_window, img_)
 
 
 def gammaDisplay(img_path: str, rep: int):
@@ -41,7 +32,6 @@ def gammaDisplay(img_path: str, rep: int):
         img = cv.imread(img_path, 1)
 
     cv.namedWindow(title_window)
-    trackbar_name = 'Gamma:'
     cv.createTrackbar(trackbar_name, title_window, 100, gamma_slider_max_val, on_trackbar)
     # Show the trackbar
     on_trackbar(100)  # Start when the img is multiply by gamma = 1 == 100 a.k.a. the original img.
@@ -51,8 +41,18 @@ def gammaDisplay(img_path: str, rep: int):
     cv.waitKey(1)
 
 
+def on_trackbar(val: int):
+    gamma = float(val) / 100
+    inv_gamma = 1000 if gamma == 0 else 1.0 / gamma
+    gammaMat = np.array([((i / float(max_pix)) ** inv_gamma) * max_pix
+                         for i in np.arange(0, max_pix + 1)]).astype("uint8")
+    img_after_gamma = cv.LUT(img, gammaMat)
+
+    cv.imshow(title_window, img_after_gamma)
+
+
 def main():
-    gammaDisplay('images/bac_con.png', 1)
+    gammaDisplay('images/sunrise.jpeg', 2)
 
 
 if __name__ == '__main__':
---
+++ myMain.py
@@ -0,0 +1,147 @@
+from matplotlib import rcParams
+
+from ex1_utils import *
+from gamma import gammaDisplay
+import numpy as np
+import matplotlib.pyplot as plt
+from cv2 import cv2 as cv
+
+import time
+
+
+def histEqDemo(img_path: str, rep: int):
+    img = imReadAndConvert(img_path, rep)
+    imgeq, histOrg, histEq = hsitogramEqualize(img)
+
+    # Display cumsum
+    cumsum = np.cumsum(histOrg)
+    cumsumEq = np.cumsum(histEq)
+    plt.gray()
+
+    plt.subplot(2, 2, 1, frameon=False)
+    plt.tick_params('x', labelbottom=False)
+    plt.tick_params('y', labelbottom=False)
+    plt.imshow(img)
+    plt.title("Before")
+
+    plt.subplot(2, 2, 2, frameon=False)
+    plt.tick_params('x', labelbottom=False)
+    plt.tick_params('y', labelbottom=False)
+    plt.imshow(imgeq)
+    plt.title("After")
+
+    plt.subplot(2, 2, (3, 4))
+    plt.plot(range(256), cumsum, 'r', label='Original')
+    plt.plot(range(256), cumsumEq, 'g', label='Equalized')
+    plt.xlabel("Intensity level", fontsize=15)
+    plt.ylabel("Probability", fontsize=15)
+    plt.legend(loc='lower right', fontsize='11')
+
+    title = "Histogram Equalization"
+    plt.suptitle(title, fontsize=19)
+    plt.show()
+
+
+def quantDemo(img_path: str, rep: int):
+    img = imReadAndConvert(img_path, rep)
+    st = time.time()
+    num2quant = 3
+    num_of_max_iter = 20
+    img_lst, err_lst = quantizeImage(img, num2quant, num_of_max_iter)
+
+    print("Time:%.2f" % (time.time() - st))
+    print("Error 0:\t %.6f" % err_lst[0])
+    print("Error last:\t %.6f" % err_lst[-1])
+    print()
+    i = err_lst.index(min(err_lst))
+    print(f'Best index of MSE list: {i}, length of the list: {len(err_lst)}')
+    print("MSE list:")
+    print(err_lst)
+    print()
+    plt.gray()
+
+    ax1 = plt.subplot(2, 2, 1, frameon=False)
+    plt.tick_params('x', labelbottom=False)
+    plt.tick_params('y', labelbottom=False)
+    plt.imshow(img)
+    plt.title("Original image")
+
+    ax2 = plt.subplot(2, 2, 2, frameon=False)
+    plt.tick_params('x', labelbottom=False)
+    plt.tick_params('y', labelbottom=False)
+    plt.imshow(img_lst[0])
+    plt.title("First iteration")
+
+    ax3 = plt.subplot(2, 2, 3, frameon=False)
+    plt.tick_params('x', labelbottom=False)
+    plt.tick_params('y', labelbottom=False)
+    plt.imshow(img_lst[-1])
+    plt.title("Last iteration")
+
+    plt.subplot(2, 2, 4)
+    plt.plot(err_lst, 'r')
+    plt.title("MSE - Graph")
+
+    title = f'Quantize image from 256 to: {num2quant}, Max iterations: {num_of_max_iter}, actual: {len(err_lst)}\n'
+    plt.suptitle(title, fontsize=14, color='b')
+    plt.show()
+
+    i = 0
+    colors = [2, 4, 8, 16, 32]
+    fig, axs = plt.subplots(2, 3, figsize=(7, 4), constrained_layout=True, sharex='all', sharey='all')
+    plt.gray()
+    fontdict = {'fontsize': 12,
+                'fontweight': 4}
+    for ax in axs.flat:
+        if i == len(colors):
+            ax.imshow(img)
+            ax.set_title("Original image", fontdict=fontdict)
+        else:
+            q, err = quantizeImage(img, colors[i], 100)
+            ax.imshow(q[i])
+            ax.set_title("Quantization of %d" % colors[i], fontdict=fontdict)
+            i += 1
+    plt.show()
+
+
+def main():
+    print("ID:", myID())
+    img_path = 'images/uluru.jpeg'
+
+    # Basic read and display
+    imDisplay(img_path, LOAD_GRAY_SCALE)
+    imDisplay(img_path, LOAD_RGB)
+
+    # Convert Color spaces
+    rgb_img = imReadAndConvert(img_path, LOAD_RGB)
+    gray_img = imReadAndConvert(img_path, LOAD_GRAY_SCALE)
+    yiq_img = transformRGB2YIQ(rgb_img)
+    i = 0
+    images = [gray_img, rgb_img, yiq_img]
+    spaces = ['Gray', 'RGB', 'YIQ']
+    fig, axs = plt.subplots(1, 3, figsize=(7, 3), constrained_layout=True, sharex='all', sharey='all')
+    plt.gray()
+    fontdict = {'fontsize': 12,
+                'fontweight': 4}
+    for ax in axs.flat:
+        ax.imshow(images[i])
+        ax.set_title(f'{spaces[i]} Color Space', fontdict=fontdict)
+        i += 1
+    title = f'Same image in several color spaces'
+    plt.suptitle(title, fontsize=18, fontweight=6)
+    # plt.show()
+
+    # # Image histEq
+    histEqDemo(img_path, LOAD_GRAY_SCALE)
+    histEqDemo(img_path, LOAD_RGB)
+
+    # Image Quantization
+    quantDemo(img_path, LOAD_GRAY_SCALE)
+    quantDemo(img_path, LOAD_RGB)
+
+    # Gamma
+    gammaDisplay(img_path, LOAD_GRAY_SCALE)
+
+
+if __name__ == '__main__':
+    main()
